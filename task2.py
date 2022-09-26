6ed0797d50a7d3741b89148927e8f69b2e444834
ZEID <zeidcode@gmail.com>
2022-09-26 22:53:49 +0400

    END

---
+++ task2.py
@@ -0,0 +1,21 @@
+def input_nums(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def check_predicates(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = input_nums(3)
+
+if check_predicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
